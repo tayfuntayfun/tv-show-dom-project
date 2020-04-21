88ed661a86c317cef190afdf637312392b34e9d9
Tayfun Aydogan <40833117+tayfuntayfun@users.noreply.github.com>
2020-04-22 00:17:35 +0100

    TvShowDOMProject_Update_100

--- script.js
+++ script.js
@@ -3,10 +3,24 @@ function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
 }
-
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
   rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  let episodes = document.createElement("div");
+  episodes.className = "episodeContainer";
+  let newEpisodes = episodeList.map(function (item) {
+    return `<div class="episode">
+            <h1 class="episodeHeader">${
+              item.name
+            } - S${item.season.toString().padStart(2, "0")}E${item.number.toString().padStart(2, "0")}</h1>
+            <img src=${item.image.medium} alt= ${item.name}
+            <p>${item.summary}</p>
+            </div>`;
+  });
+  episodes.innerHTML = newEpisodes;
+  rootElem.appendChild(episodes);
+  // console.log(episodes);
+  // console.log(episodeList);
+  
 }
-
 window.onload = setup;
