37d79188d1eaf273801b8117b83186a3e625ad11
Tayfun Aydogan <40833117+tayfuntayfun@users.noreply.github.com>
2020-04-25 19:41:24 +0100

    Update 300

--- script.js
+++ script.js
@@ -7,8 +7,11 @@ function setup() {
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
   //Create search feature;
-  rootElem.innerHTML = `<span id="search-bar">Search all episodes</span>
-                        <input type="search" id="search-content" placeholder="Search for..">`; 
+  rootElem.innerHTML = `<div id="search-episodes">
+  <span class="search-bar">Search all episodes</span>
+  <select id="episode-list"></select>
+  <input type="search" id="search-episodes" placeholder="Search for..">
+  </div>`; 
   
   //div creation for all episodes
   let episodes = document.createElement("div");
@@ -17,15 +20,32 @@ function makePageForEpisodes(episodeList) {
   rootElem.appendChild(episodes)
   
   //Search Button function
-  let getSearchButton = document.getElementById("search-content");
-  getSearchButton.addEventListener("input", function () {
-  let filteredEpisodes = episodeList.filter((key) => key.summary.toLowerCase().includes(getSearchButton.value) ||
-  key.name.toLowerCase().includes(getSearchButton.value)
-  );
-  episodes.innerHTML = createNewList(filteredEpisodes);
-});
-}
-function  createNewList(episodeList){ 
+  const getInputField = document.getElementById("search-episodes");
+
+  // Dropdown rolling search
+  const dropDownSearchMenu = document.querySelector("#episode-list");
+  dropDownSearchMenu.addEventListener("change", function (event) {
+    const episodeId = event.target.value;
+    const episodesFilteredById = episodeList.filter(
+      (episode) => episode.id == episodeId
+    );
+    episodes.innerHTML = createNewList(episodesFilteredById);
+  });
+
+  dropDownSearchMenu.innerHTML = createDropDownMenu(episodeList);
+//DropDown menu
+  function createDropDownMenu(episodeList) {
+    return episodeList
+      .map(function (item) {
+        return `<option value =${item.id}>
+    S${item.season.toString().padStart(2,"0")}
+    E${item.number.toString().padStart(2, "0")} 
+    ${item.name}</option>`;
+      })
+    .join("");
+  }
+
+  function  createNewList(episodeList){ 
   return episodeList.map(function (item) {
      return `<div class="episode">
       <h1 class="episodeHeader">${item.name} - S${item.season.toString().padStart(2, "0")}E${item.number.toString().padStart(2, "0")}</h1>
@@ -33,6 +53,7 @@ function  createNewList(episodeList){
       ${item.summary}
       </div>`;
   }).join('');
+}
 
 }
 
